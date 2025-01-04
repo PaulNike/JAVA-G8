c568c4fa72872f910e22445730fd9ec6a2578d3e
NikeRodriguez <nike_1109@outlook.es>
2025-01-03 21:45:11 -0500

    Tema: Aprendiendo a USar OpenFeign

---
+++ apis-externas/src/main/java/com/codigo/apis_externas/ApisExternasApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.apis_externas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisExternasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisExternasApplication.class, args);
+	}
+
+}
